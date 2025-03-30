dd38bc2c38219dadb6507746789faca3baab4182
= <=>
2025-03-30 21:22:12 +0545

    training v2 print statement has been added

--- src/main/java/com/exosolve/inheritance/RunPayment.java
+++ src/main/java/com/exosolve/inheritance/RunPayment.java
@@ -5,7 +5,7 @@ public class RunPayment {
     public static void main(String[] args) {
 
         Payment pay = new AdvancePayment(1,"RIYA");
-
+        System.out.println("Training v2 feature has been added");
         pay.initiatePayment();
         System.out.println("Hello there this is a ne change eeeeeee");
         Payment payPayal = new PaypalPayment();
