6e09648341d0da88eba98f1c0e47cda98393f165
= <=>
2025-04-06 06:54:17 +0545

    content for employee and ArrayListExample has been added

--- src/main/java/com/exosolve/collectionframework/ArrayListExample.java
+++ src/main/java/com/exosolve/collectionframework/ArrayListExample.java
@@ -1,2 +1,31 @@
-package com.exosolve.collectionframework;public class ArrayListExample {
+package com.exosolve.collectionframework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+
+        Employee eone = new Employee(1,"Aruna",120,"female");
+        Employee etwo = new Employee(3,"Arun",120,"male");
+        Employee ethree = new Employee(367,"Ark",150,"male");
+
+
+        List<Employee> list = new ArrayList<>();
+        list.add(eone);
+        list.add(etwo);
+        list.add(ethree);
+
+        System.out.println(list.size());
+        System.out.println(list.contains(etwo));
+        System.out.println(list.set(1,new Employee(368,"Villan",170,"male")));
+
+
+        for (Employee employee:list){
+            System.out.println("Id: "+ employee.getEmpId() + " Name : "+ employee.getEmpName() + " Salary : " + employee.getEmpSalary() + " Gender :  "+ employee.getGender());
+        }
+
+
+
+    }
 }
--- src/main/java/com/exosolve/collectionframework/Employee.java
+++ src/main/java/com/exosolve/collectionframework/Employee.java
@@ -1,2 +1,50 @@
-package com.exosolve.collectionframework;public class Employee {
+package com.exosolve.collectionframework;
+
+public class Employee {
+    private long empId;
+    private String empName;
+    private double empSalary;
+    private String gender;
+
+
+    public Employee(long empId, String empName, double empSalary, String gender) {
+        this.empId = empId;
+        this.empName = empName;
+        this.empSalary = empSalary;
+        this.gender = gender;
+    }
+
+
+
+    public long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getEmpSalary() {
+        return empSalary;
+    }
+
+    public void setEmpSalary(double empSalary) {
+        this.empSalary = empSalary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
