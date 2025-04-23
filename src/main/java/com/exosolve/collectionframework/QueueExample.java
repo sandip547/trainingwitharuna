3dd1d34494ab8043f7f1097e52f5e24b220200c2
= <=>
2025-04-23 06:09:56 +0545

    hashmap

--- src/main/java/com/exosolve/collectionframework/QueueExample.java
+++ src/main/java/com/exosolve/collectionframework/QueueExample.java
@@ -1,2 +1,23 @@
-package com.exosolve.collectionframework;public class QueueExample {
+package com.exosolve.collectionframework;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<String> queue = new ArrayDeque<>();
+
+        queue.add("Hello");
+        queue.add("there");
+        queue.add("sun");
+
+        System.out.println("initial queue:" + queue);
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println("queue after processin" + queue);
+        System.out.println(queue.poll());
+
+
+    }
 }
